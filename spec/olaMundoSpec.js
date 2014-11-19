514cbef29ad1ab2fe22327dd3bbe2086053c131f
kri8or <davmar@outlook.pt>
2014-11-19 20:48:02 +0000

    files added and hello world working

---
+++ spec/olaMundoSpec.js
@@ -0,0 +1,5 @@
+describe("ola mundo", function() {
+	it("says ola", function() {
+		expect(olaMundo()).toEqual("ola mundao!!");
+	});
+});
---
+++ src/olaMundoWhatever.js
@@ -0,0 +1,3 @@
+function olaMundo(){
+	return "ola mundao!!";
+};
