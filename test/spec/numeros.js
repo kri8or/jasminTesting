b902fec53507d1959febca91785e98f1259d3258
kri8or <davmar@outlook.pt>
2014-11-20 07:38:09 +0000

    creating, hiding and pending tests and test suits

--- test/spec/numeros.js
+++ test/spec/numeros.js
@@ -3,8 +3,37 @@ define(['numeros'],function(numeros){
 	'use strict';
 
 	describe('O modulo dos numeros',function(){
-		describe('metodo adicionar',function(){
 		
+		xdescribe('this is how to hide a complete test suite',function(){
+			
+			it('whatever', function(){
+				
+			});
+			it('whatever1', function(){
+				
+			});
+			it('whatever2', function(){
+				
+			});			
 		});
+
+
+		describe('metodo adicionar',function(){
+			
+			it('this is how you make a pending test', function(){
+				pending();
+			});
+			xit('this is another way how you make a pending test', function(){
+				
+			});
+
+			it('should accept one or more args and return the sum them', function(){
+				pending();
+			});
+			
+		});
+
+		
+
 	});
 });
