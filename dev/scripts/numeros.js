d5e9aad260bdb8b120f2feff7b562fd45822e702
kri8or <davmar@outlook.pt>
2014-11-20 09:10:19 +0000

    css greener, less repetition is tests, added more tests for case of string inputs

--- dev/scripts/numeros.js
+++ dev/scripts/numeros.js
@@ -11,12 +11,14 @@ define(function(){
 		total = 0;
 
 		operands.forEach(function(value){
+			
+			if (typeof value === 'string'){
+				value = parseInt(value,10) || 0;
+			}
 			total+=value;
 		});
 
 		return total;
-
-
 	};
 
 	return self;
--- test/spec/numeros.js
+++ test/spec/numeros.js
@@ -28,21 +28,44 @@ define(['numeros'],function(numeros){
 
 		describe('metodo adicionar',function(){
 			
+			var output;
+
+			beforeEach(function(){
+				this.numInput1 =1;
+				this.numInput2 =2;
+				this.stringInput1 ='1';
+				this.stringInput2 ='ups';
+			});
 			
 
 			it('should accept one or more args and return the sum them', function(){
-				
-				//arrange
-				var output,
-					input1=1,
-					input2=2;
-				
+					
 				//act	
-				output = numeros.add(input1,input2);
+				output = numeros.add(this.numInput1,this.numInput2);
 
 				//assert
 				expect(output).toEqual(3);
 				expect(output).not.toEqual(4);
+			});
+
+			it('should try to parse an integer when a string is in input', function(){
+				
+				//act	
+				output = numeros.add(this.numInput1,this.numInput2,this.stringInput1);
+
+				//assert
+				expect(output).toEqual(4);
+			});
+
+			it('should ignore strings not parsable to integers', function(){
+				
+				
+				//act	
+				output = numeros.add(this.numInput1,this.numInput2,this.stringInput2);
+
+				//assert
+				expect(output).toEqual(3);
+				
 			});
 			
 		});
