77162fa4d6dc5d77ea3d450abe7dfc3b9ae748dc
kri8or <davmar@outlook.pt>
2014-11-20 08:46:20 +0000

    passing the test from last commit

--- dev/scripts/numeros.js
+++ dev/scripts/numeros.js
@@ -5,6 +5,17 @@ define(function(){
 	var self = {};
 	
 	self.add=function add(){
+		
+		//operands will be an array containing the paremeters of add()
+		var operands= Array.prototype.slice.call(arguments),
+		total = 0;
+
+		operands.forEach(function(value){
+			total+=value;
+		});
+
+		return total;
+
 
 	};
 
